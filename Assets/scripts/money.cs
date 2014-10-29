bd3467f4e9420e387c878adca8070450e996726e
lproductions <l.productionsuk@gmail.com>
2014-10-29 12:45:20 +0000

    added stuff

--- Assets/scripts/money.cs
+++ Assets/scripts/money.cs
@@ -4,12 +4,19 @@ using System.Collections;
 public class money : MonoBehaviour {
 
 	// Use this for initialization
+    static double money = 10000;
+    static long moneypersecond;
 	void Start () {
-	
+        StartCoroutine(moneypersecondsomething());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        
 	}
+    IEnumerator moneypersecondsomething() {
+        money += moneypersecond / 10;
+        yield return new WaitForSeconds(0.1f);
+        moneypersecondsomething();
+    }
 }
