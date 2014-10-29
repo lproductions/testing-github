978de0eecec81f1826fc0a0b2a8e2cd2cfa469fe
lproductions <l.productionsuk@gmail.com>
2014-10-29 15:11:27 +0000

    added save.cs

--- Assets/scripts/save.cs
+++ Assets/scripts/save.cs
@@ -2,7 +2,18 @@
 using System.Collections;
 
 public class save : MonoBehaviour {
-
+    public int taxi1 = 1;
+    public int busowned1 = 0;
+    public int boatowned1 = 0;
+    public int boatowned2 = 0;
+    public int trainowned1 = 0;
+    public int trainowned2 = 0;
+    public int undergroundtrainowned1 = 0;
+    public int planeowned1 = 0;
+    public int planeowned2 = 0;
+    
+    public int planeowned3 = 0;
+    public static PlayerPrefs pp;
 	// Use this for initialization
 	void Start () {
 	
