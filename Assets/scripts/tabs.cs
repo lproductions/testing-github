4af7e3b32f468356c6ac220ccfa436adb64bd2c5
lproductions <l.productionsuk@gmail.com>
2014-10-29 18:48:57 +0000

    added tab selector

---
+++ Assets/scripts/tabs.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tabs : MonoBehaviour {
+    public static int tabselected = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+   public  void settab(int tab) {
+
+        tabselected = tab;
+    }
+}
